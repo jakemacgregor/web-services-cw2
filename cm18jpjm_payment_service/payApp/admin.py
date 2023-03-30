9fd724a6c80df033944c63469ffae769ebc727a3
Jake MacGregor <jakemacgregorspare@gmail.com>
2023-03-30 10:31:30 +0100

    Add models

--- cm18jpjm_payment_service/payApp/admin.py
+++ cm18jpjm_payment_service/payApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import PaymentMethodDetails, OrderDetails, ItemDetails
 
 # Register your models here.
+admin.site.register(PaymentMethodDetails)
+admin.site.register(OrderDetails)
+admin.site.register(ItemDetails)
--- cm18jpjm_payment_service/payApp/models.py
+++ cm18jpjm_payment_service/payApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class PaymentMethodDetails(models.Model):
+    card_number = models.BigIntegerField
+    cvv = models.IntegerField
+    expiry_date = models.DateField
+    cardholder_name = models.CharField(max_length=30)
+
+
+class OrderDetails(models.Model):
+    total_price = models.DecimalField
+    payment_method = models.ForeignKey(PaymentMethodDetails, on_delete=models.PROTECT)
+    payment_status = models.IntegerField
+
+
+class ItemDetails(models.Model):
+    order = models.ForeignKey(OrderDetails, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=30)
+    item_price = models.DecimalField
+
